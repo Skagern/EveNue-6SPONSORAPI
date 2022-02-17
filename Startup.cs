d3206b45eff9be5897b05f7878d92bb22b900971
Skagern <Skagern@hotmail.com>
2022-02-17 14:50:20 +0100

    RadändringServerkoppling

--- Startup.cs
+++ Startup.cs
@@ -42,12 +42,12 @@ namespace GruppNrSexAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GruppNrSexAPI v1"));
-            }
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("v1/swagger.json", "GruppNrSexAPI v1"));
+            //}
 
             app.UseRouting();
 
